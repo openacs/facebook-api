d8fe8f3ca3db79efcc324894a8fd01f3a5b131ce
Hamilton Chua <hamilton.chua@gmail.com>
2009-11-03 06:18:41 +0000

    update facebook-api to version 0.2d, add support for photos

--- sql/postgresql/facebook-api-create.sql
+++ sql/postgresql/facebook-api-create.sql
@@ -1,14 +1,22 @@
 create table fb_users (
-       uid integer,
-       last_friends_update timestamptz
+    uid integer not null primary key,
+    last_friends_update timestamptz,
+    auth_token text,
+    session_key text,
+    session_expires text
 );
 
 create table fb_friends (
-       uid integer,
-       friend_uid integer
+    uid integer,
+    friend_uid integer
 );
 
 create table fb_groups (
-       gid integer,
-       uid integer
+    gid integer,
+    uid integer
+);
+
+create table oacs_fb_user_map (
+    user_id integer not null primary key,
+    uid integer
 );
---
+++ sql/postgresql/facebook-api-drop.sql
@@ -0,0 +1,4 @@
+drop table fb_users;
+drop table fb_friends;
+drop table fb_groups;
+drop table oacs_fb_user_map;
